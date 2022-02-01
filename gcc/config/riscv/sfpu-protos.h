8ef80de0c38076c4074f508a9cb415940389d74d
Paul Keller <pkeller@tenstorrent.com>
2022-02-01 17:34:29 +0000

    TT: Add comment in asm files for non-imm instructions

--- gcc/config/riscv/sfpu-protos.h
+++ gcc/config/riscv/sfpu-protos.h
@@ -3,16 +3,21 @@
 
 #include "sfpu_ops.h"
 
+#define riscv_sfpu_regno(operand) (REGNO(operand) - SFPU_REG_FIRST)
+
+extern const char* riscv_sfpu_lv_regno_str(char *str, rtx operand);
+
 extern rtx riscv_sfpu_clamp_signed(rtx v, unsigned int mask);
 extern rtx riscv_sfpu_clamp_unsigned(rtx v, unsigned int mask);
 extern rtx riscv_sfpu_gen_const0_vector();
+
 extern void riscv_sfpu_emit_sfpassignlr(rtx dst, rtx lr);
 extern void riscv_sfpu_emit_nonimm_dst(rtx buf_addr, rtx dst, int nnops, rtx dst_lv,
 				       rtx imm, int base, int lshft, int rshft, int dst_shft);
 extern void riscv_sfpu_emit_nonimm_dst_src(rtx buf_addr, rtx dst, int nnops, rtx dst_lv, rtx src, rtx imm, int base, int lshft, int rshft, int dst_shft, int src_shft);
 extern void riscv_sfpu_emit_nonimm_src(rtx buf_addr, rtx src, int nnops, rtx imm, int base, int lshft, int rshft, int src_shft);
 extern void riscv_sfpu_emit_nonimm_store(rtx buf_addr, rtx src, int nnops, rtx imm, int base, int lshft, int rshft, int src_shft);
-extern char const * riscv_sfpu_output_nonimm_store_and_nops(char *sw, int nnops, rtx operands[]);
+extern char const * riscv_sfpu_output_nonimm_store_and_nops(const char *sw, int nnops, rtx operands[]);
 
 extern void riscv_sfpu_emit_sfpload(rtx dst, rtx lv, rtx addr, rtx mod, rtx imm);
 extern void riscv_sfpu_emit_sfploadi(rtx dst, rtx lv, rtx addr, rtx mod, rtx imm);
