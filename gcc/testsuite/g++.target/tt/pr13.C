99e503b605c52b97909ef51440440817b084764c
Nathan Sidwell <nsidwell@tenstorrent.com>
2024-08-27 18:10:41 +0000

    [pr13] Use a graph walk for SFPNOP insertion
    We can't just look at the immediate successor blocks, but must walk
    the graph until we find a reachable dependent insn. Obviously we need
    to be careful of loops -- hence marking the visited bbs.  Note that
    the first BB might itself be reachable, and we want to check it has no
    use of the result. If we reach a non-dependent SPU insn, we do not
    need a NOP.

---
+++ gcc/testsuite/g++.target/tt/pr13.C
@@ -0,0 +1,60 @@
+// Wormhole test to make sure SFPNOPS are inserted, even when the
+// dependency is several BB's away. Somewhat awkward to arrange such a
+// testcase, this is probably fragile
+
+// { dg-do compile }
+// { dg-additional-options "-mwormhole -march=rv32imw -mtune=rvtt-b1 -mabi=ilp32 -O3" }
+
+using vec_t = float __attribute__((vector_size(64 * sizeof(float))));
+
+inline void calculate_power_iterative(unsigned exponent) {
+#pragma GCC unroll 8
+  for (int d = 0; d < 8; d++) {
+    vec_t in = __builtin_rvtt_wh_sfpload(nullptr, 0, 3, 0, 0, 0);
+    vec_t acc = __builtin_rvtt_wh_sfpxloadi(nullptr, 18, 0x3f800000, 0, 0);
+    for (unsigned i = 0; i < exponent; i++)
+      acc = __builtin_rvtt_wh_sfpassign_lv(
+	      acc, __builtin_rvtt_wh_sfpmul(acc, in, 0));
+
+    __builtin_rvtt_wh_sfpstore(nullptr, acc, 12, 3, 0, 0, 0);
+    __builtin_rvtt_sfpincrwc(0, 2, 0, 0);
+  }
+}
+
+inline void llk_math_eltwise_unary_sfpu_params(unsigned dst_index,
+					       int, unsigned arg) {
+#pragma GCC unroll 0
+  for (int face = 0; face < 4; face++)
+    calculate_power_iterative(arg);
+}
+
+inline void llk_math_eltwise_unary_sfpu_power(unsigned dst_index, int pow = 0,
+					      int vector_mode = 4) {
+  llk_math_eltwise_unary_sfpu_params(dst_index, 4, pow);
+}
+
+__attribute__((always_inline))
+inline void power_tile(unsigned, unsigned param0) {
+  llk_math_eltwise_unary_sfpu_power(0, param0);
+}
+  
+extern unsigned *args;
+
+static void math_main() {
+  int ix = 0;
+  const auto height = args[ix++];
+  const auto width = args[ix++];
+  const auto p = args[ix++];
+
+  for (unsigned jx = 0; jx < height; jx++)
+    for (unsigned ix = 0; ix < width; ix++)
+      power_tile(0, p);
+}
+
+void run_kernel() {
+  math_main();
+}
+
+// Same number of SFPMULs as SFPNOPs
+// { dg-final { scan-assembler-times {SFPMUL[^A-Z]} 8 } }
+// { dg-final { scan-assembler-times {SFPNOP[^A-Z]} 8 } }
