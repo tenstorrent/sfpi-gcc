ed406fb6731206100ab8eaf17b5e055d4f7e6904
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-02-13 18:54:20 +0000

    #14604: Add BH sfpmad sign inversion capability

---
+++ gcc/testsuite/g++.target/tt/sfpi/fpsub-14604-wh.C
@@ -0,0 +1,86 @@
+// { dg-options "-mcpu=tt-wh -O2 -I [SFPI]/include -fno-exceptions -fno-rtti" }
+
+// Verify we notice a - b is a + -1.0 * b
+
+namespace ckernel{
+    unsigned *instrn_buffer;
+}
+#include <sfpi.h>
+
+using namespace sfpi;
+
+void sub1() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat r;
+
+  r = a - b;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z4sub1v:\n\tSFPMAD	L3, L11, L1, L0, 0\n\tret\n} } }
+
+void sub2() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+
+  b -= a;
+  
+  l_reg[LRegs::LReg3] = b;
+}
+// { dg-final { scan-assembler {\n_Z4sub2v:\n\tSFPMAD	L3, L11, L0, L1, 0\n\tret\n} } }
+
+void sub3() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat r;
+
+  r = -a - b;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z4sub3v:\n\tSFPMOV	L0, L0, 1\n\tSFPMAD	L3, L11, L1, L0, 0\n\tret\n} } }
+
+void sub4() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat r;
+
+  v_if (a < b) {
+    r = a - b;
+  } v_else {
+    r = b - a;
+  } v_endif;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z4sub4v:\n\tSFPMAD	L3, L1, L11, L0, 0\n\tSFPNOP\n\tSFPSETCC	L3, 0, 0\n\tSFPMAD	L3, L11, L1, L0, 0\n\tSFPCOMPC\n\tSFPMAD	L3, L11, L0, L1, 0\n\tSFPENCC	3, 10\n\tret\n} } }
+
+void sub5() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+
+  v_if (a < b) {
+    b -= a;
+  } v_else {
+    b -= -a;
+  } v_endif;
+  
+  l_reg[LRegs::LReg3] = b;
+}
+// { dg-final { scan-assembler {\n_Z4sub5v:\n\tSFPMAD	L2, L1, L11, L0, 0\n\tSFPNOP\n\tSFPSETCC	L2, 0, 0\n\tSFPMAD	L1, L11, L0, L1, 0\n\tSFPCOMPC\n\tSFPMOV	L0, L0, 1\n\tSFPMAD	L1, L11, L0, L1, 0\n\tSFPENCC	3, 10\n\tSFPMOV	L3, L1, 2\n\tret\n} } }
+
+void sub6() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat r;
+
+  v_if (a < b) {
+    r = -a - b;
+  } v_else {
+    r = a - b;
+  } v_endif;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z4sub6v:\n\tSFPMAD	L3, L1, L11, L0, 0\n\tSFPNOP\n\tSFPSETCC	L3, 0, 0\n\tSFPMOV	L3, L0, 1\n\tSFPMAD	L3, L11, L1, L3, 0\n\tSFPCOMPC\n\tSFPMAD	L3, L11, L1, L0, 0\n\tSFPENCC	3, 10\n\tret\n} } }
---
+++ gcc/testsuite/g++.target/tt/sfpi/muladd-14604-wh.C
@@ -0,0 +1,80 @@
+// { dg-options "-mcpu=tt-wh -O2 -I [SFPI]/include -fno-exceptions -fno-rtti" }
+
+namespace ckernel{
+    unsigned *instrn_buffer;
+}
+#include <sfpi.h>
+
+using namespace sfpi;
+
+void muladd() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat c = l_reg[LRegs::LReg2];
+  vFloat r;
+
+  r = b * c + a;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z6muladdv:\n\tSFPMAD	L3, L1, L2, L0, 0\n\tret\n} } }
+
+void mulsub() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat c = l_reg[LRegs::LReg2];
+  vFloat r;
+
+  r = b * c - a;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z6mulsubv:\n\tSFPMOV	L0, L0, 1\n\tSFPMAD	L3, L1, L2, L0, 0\n\tret\n} } }
+
+void negmuladd() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat c = l_reg[LRegs::LReg2];
+  vFloat r;
+
+  r = b * -c + a;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z9negmuladdv:\n\tSFPMOV	L2, L2, 1\n\tSFPMAD	L3, L1, L2, L0, 0\n\tret\n} } }
+
+void negmulsub() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat c = l_reg[LRegs::LReg2];
+  vFloat r;
+
+  r = b * -c - a;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z9negmulsubv:\n\tSFPMOV	L2, L2, 1\n\tSFPMOV	L0, L0, 1\n\tSFPMAD	L3, L1, L2, L0, 0\n\tret\n} } }
+
+void negmuladd2() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat c = l_reg[LRegs::LReg2];
+  vFloat r;
+
+  r = -b * c + a;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z10negmuladd2v:\n\tSFPMOV	L1, L1, 1\n\tSFPMAD	L3, L1, L2, L0, 0\n\tret\n} } }
+
+void negmulsub2() {
+  vFloat a = l_reg[LRegs::LReg0];
+  vFloat b = l_reg[LRegs::LReg1];
+  vFloat c = l_reg[LRegs::LReg2];
+  vFloat r;
+
+  r = -b * c - a;
+  
+  l_reg[LRegs::LReg3] = r;
+}
+// { dg-final { scan-assembler {\n_Z10negmulsub2v:\n\tSFPMOV	L1, L1, 1\n\tSFPMOV	L0, L0, 1\n\tSFPMAD	L3, L1, L2, L0, 0\n\tret\n} } }
