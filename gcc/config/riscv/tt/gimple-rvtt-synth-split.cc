11f38d2a3520886984d7869cacf68ad706b003a1
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-06-26 08:04:58 +0000

    Fix Copyright
    There is no FSF assignment in place

--- gcc/config/riscv/tt/gimple-rvtt-synth-split.cc
+++ gcc/config/riscv/tt/gimple-rvtt-synth-split.cc
@@ -1,7 +1,8 @@
 /* Pass to generate SFPU synth-opcode and opcode synth sequences for
    currently-non-constant operands.
-   Copyright (C) 2022 Free Software Foundation, Inc.  Contributed by
-   Paul Keller (pkeller@tenstorrent.com).
+   Copyright (C) 2022-2025 Tenstorrent Inc.
+   Originated by Paul Keller (pkeller@tenstorrent.com).
+   Rewritten Nathan Sidwell (nsidwell@tenstorrent.com, nathan@acm.org).
 
 This file is part of GCC.
 
