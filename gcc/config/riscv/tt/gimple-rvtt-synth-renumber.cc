4ed03bc2c599a8e9d7248cc6338cc3c7ea42acac
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-07-01 14:37:02 +0000

    detect ivopts

--- gcc/config/riscv/tt/gimple-rvtt-synth-renumber.cc
+++ gcc/config/riscv/tt/gimple-rvtt-synth-renumber.cc
@@ -114,7 +114,20 @@ transform (function *fn)
   // synth_opcode -> nullptr, num-adds
   // add -> synth_opcode, (remapped id or 0) << 1 | op2
   // nonimm_use -> add, nonimm-pos
-  std::unordered_map<gimple *, std::pair<gimple *, unsigned>> map;
+  struct use_info
+  {
+    gimple *opcode_stmt = nullptr;
+    gimple *add_stmt = nullptr;
+    unsigned count = 0; // opcode_stmt: count of adds then new id
+                        // use stmt: nonimm pos
+    bool flag = false;  // add:rhs2
+
+    use_info () = default;
+    use_info (unsigned pos) : count (pos) {}
+    use_info (gimple *op, gimple *def, bool f)
+      : opcode_stmt (op), add_stmt (def != op ? def : nullptr), flag (f) {}
+  };
+  std::unordered_map<gimple *, use_info> map;
   for (auto &def_use : synths)
     {
       if (!def_use.uses || !def_use.uses->next)
@@ -126,102 +139,111 @@ transform (function *fn)
       map.clear ();
       // Insert all the defs
       for (auto *def = def_use.defs; def; def = def->next)
-	map.insert ({def->call, {nullptr, 0}});
+	map.insert ({def->call, use_info ()});
       // Insert all the uses
-      for (auto *use = def_use.uses; use; use = use->next)
-	map.insert ({use->call, {nullptr, use->pos}});
-
-      // Process all the uses, nearly all should meet an add, which in turn
-      // should meet a def. (We won't meet an add is cprop has
-      // determined the addedn is zero, Huh? why is the nonimm not imm
-      // then?
-      unsigned num_adds = 0;
-      tree synth_opcode_decl = rvtt_get_insn_data (rvtt_insn_data::synth_opcode)->decl;
+      unsigned num_uses = 0;
       for (auto *use = def_use.uses; use; use = use->next)
 	{
-	  if (map.find (use->call)->second.first)
-	    // Already met this processing another one (so they share
-	    // the add).
-	    continue;
+	  map.insert ({use->call, use_info (use->pos)});
+	  num_uses++;
+	}
 
-	  tree arg = gimple_call_arg (use->call, use->pos + 1);
-	  gcc_assert (TREE_CODE (arg) == SSA_NAME);
-	  gimple *def_stmt = SSA_NAME_DEF_STMT (arg);
-	  gassign *add = dyn_cast <gassign *> (def_stmt);
-	  if (!add)
+      // Process all the uses, nearly all should meet an add, which in turn
+      // should meet a def.  There are two other case.
+      // 1) See below abour sfpxicmps and friends
+      // 2) a partially unrolled loop can cascade adds after an
+      // ivopt.  We need to record the whole chain in order to split it.
+      unsigned num_adds = 0;
+      unsigned total_adds = 0;
+      tree synth_opcode_decl = rvtt_get_insn_data (rvtt_insn_data::synth_opcode)->decl;
+      auto build_graph = [&] (auto &self, gimple *opcode_stmt, gimple *def_stmt, bool is_indirect)
+      {
+	tree ssa_var = gimple_get_lhs (def_stmt);
+	imm_use_iterator iter;
+	gimple *use_stmt;
+
+	// FIXME: Can we use quick iterator here?
+	FOR_EACH_IMM_USE_STMT (use_stmt, iter, ssa_var)
+	  {
+	    if (is_gimple_call (use_stmt))
+	      {
+		// a final use
+		auto slot = map.find (use_stmt);
+		if (slot == map.end())
+		  {
+		    debug_gimple_stmt (opcode_stmt);
+		    debug_gimple_stmt (def_stmt);
+		    gcc_assert (false);
+		  }
+		if (opcode_stmt == def_stmt)
+		  // sfpxicmps and friends cause the add to be elided by
+		  // setting the mask to zero. Things get reconstituted in
+		  // the expand pass.
+		  // FIXME: That's hokey as it prevents renumbering we're
+		  // trying to achieve here. We should do better.
+		  return true;
+
+		slot->second.opcode_stmt = opcode_stmt;
+		slot->second.add_stmt = def_stmt;
+		num_uses--;
+	      }
+	    else if (auto *add_stmt = dyn_cast <gassign *> (use_stmt))
+	      {
+		// an add
+		gcc_assert (gimple_assign_rhs_code (add_stmt) == PLUS_EXPR);
+		tree opcode_arg = gimple_assign_rhs1 (add_stmt);
+		bool is_op2 = opcode_arg != ssa_var;
+		gcc_assert (!is_op2 || gimple_assign_rhs2 (add_stmt) == ssa_var);
+		if (opcode_stmt != def_stmt)
+		  {
+		    // If this is an add of an add, that's the ivopt chain
+		    gcc_assert (!is_op2
+				&& TREE_CODE (gimple_assign_rhs2 (add_stmt)) == INTEGER_CST);
+		    // Treat as complex for now
+		    return true;
+		  }
+		auto [iter, inserted] = map.insert ({add_stmt, use_info (opcode_stmt, def_stmt, is_op2)});
+		gcc_assert (inserted);
+		num_adds++;
+
+		if (self (self, opcode_stmt, add_stmt, opcode_stmt != def_stmt))
+		  return true;
+	      }
+	    else if (gimple_code (use_stmt) != GIMPLE_DEBUG)
+	      {
+		debug_gimple_stmt (opcode_stmt);
+		debug_gimple_stmt (def_stmt);
+		debug_gimple_stmt (use_stmt);
+		gcc_assert (false);
+	      }
+	  }
+	return false;
+      };
+
+      for (auto *def = def_use.defs; def; def = def->next)
+	{
+	  gimple *opcode_stmt = def->call;
+
+	  num_adds = 0;
+	  if (build_graph (build_graph, opcode_stmt, opcode_stmt, false))
 	    {
-	      // sfpxicmps and friends cause the add to be elided by
-	      // setting the mask to zero. Things get reconstituted in
-	      // the expand pass.
-	      // FIXME: That's hokey as it prevents renumbering we're
-	      // trying to achieve here. We should fix it.
-	      gcc_assert (is_gimple_call (def_stmt)
-			  && gimple_call_fndecl (def_stmt) == synth_opcode_decl);
 	      complex = true;
 	      break;
 	    }
-
-	  gcc_assert (gimple_assign_rhs_code (add) == PLUS_EXPR);
-
-	  // Find all the uses of the ssa-var (which will include
-	  // this one).
-	  {
-	    imm_use_iterator iter;
-	    gimple *stmt;
-	    // FIXME: Can we use quick iterator here?
-	    FOR_EACH_IMM_USE_STMT (stmt, iter, arg)
-	      {
-		if (gimple_code (stmt) == GIMPLE_DEBUG)
-		  continue;
-		auto slot = map.find (stmt);
-		if (slot == map.end())
-		  {
-		    // ASSERT?
-		    complex = true;
-		    break;
-		  }
-		else
-		  slot->second.first = add;
-	      }
-	    if (complex)
-	      break;
-	  }
-
-	  // Find the synth_opcode providing the add's input, update
-	  // that
-	  bool op2 = false;
-	  tree opcode = gimple_assign_rhs1 (add);
-	  gcall *synth = nullptr;
-	  if (TREE_CODE (opcode) == SSA_NAME)
-	    synth = dyn_cast <gcall *> (SSA_NAME_DEF_STMT (opcode));
-	  if (!synth || gimple_call_fndecl (synth) != synth_opcode_decl)
-	    {
-	      op2 = true;
-	      opcode = gimple_assign_rhs2 (add);
-	      if (TREE_CODE (opcode) == SSA_NAME)
-		synth = dyn_cast <gcall *> (SSA_NAME_DEF_STMT (opcode));
-	      else
-		{
-		  debug_gimple_stmt (use->call);
-		  debug_gimple_stmt (add);
-		  gcc_assert (false);
-		}
-	    }
-	  auto synth_slot = map.find (synth);
-	  gcc_assert (synth_slot != map.end ());
-	  synth_slot->second.second++;
-
-	  // Insert add into the map -- it cannot already be there
-	  auto [iter, inserted] = map.insert ({add, {synth, op2}});
-	  gcc_assert (inserted);
-	  num_adds++;
+	  map.find (opcode_stmt)->second.count += num_adds;
+	  total_adds += num_adds;
 	}
 
+      // split?
       if (complex)
 	// Too complex, don't touch.
 	continue;
 
-      if (num_adds == 1)
+      // Make sure we found all the uses
+      gcc_assert (num_uses == 0);
+
+      if (total_adds == 1)
+	// Only one add, no need to split
 	continue;
 
       // Multiple adds, (maybe) renumber
@@ -231,16 +253,18 @@ transform (function *fn)
       for (auto &mapping : map)
 	{
 	  if (!is_gimple_call (mapping.first))
+	    // An add
 	    continue;
 	  if (gimple_call_fndecl (mapping.first) != synth_opcode_decl)
+	    // A use
 	    continue;
 
-	  if (!mapping.second.second)
-	    // no uses
+	  if (!mapping.second.count)
+	    // no adds uses this -- can we get that?
 	    continue;
 
 	  // We do not need to renumber the first use.
-	  if (first && mapping.second.second == 1)
+	  if (first && mapping.second.count == 1)
 	    {
 	      first = false;
 	      continue;
@@ -252,6 +276,9 @@ transform (function *fn)
 	    {
 	      if (!is_gimple_assign (add.first))
 		continue;
+	      if (add.second.add_stmt)
+		gcc_unreachable (); // for the moment
+
 	      if (first)
 		{
 		  first = false;
@@ -268,13 +295,13 @@ transform (function *fn)
 	      update_stmt (new_stmt);
 	      gsi_insert_after (&gsi, new_stmt, GSI_NEW_STMT);
 
-	      if (add.second.second)
+	      if (add.second.flag)
 		gimple_assign_set_rhs2 (add.first, new_op);
 	      else
 		gimple_assign_set_rhs1 (add.first, new_op);
 	      update_stmt (add.first);
 
-	      add.second.second = unique_id << 1;
+	      add.second.count = unique_id;
 	      renumbered = true;
 	    }
 	}
@@ -285,14 +312,17 @@ transform (function *fn)
 	    {
 	      if (!is_gimple_call (mapping.first))
 		continue;
-	      if (gimple_call_fndecl (mapping.first) == synth_opcode_decl)
-		continue;
+	      if (!mapping.second.add_stmt)
+		{
+		  gcc_assert (gimple_call_fndecl (mapping.first) == synth_opcode_decl);
+		  continue;
+		}
 
 	      // A use, see if it needs renumbering
-	      auto add = map.find (mapping.second.first);
+	      auto add = map.find (mapping.second.add_stmt);
 	      gcc_assert (add != map.end ());
-	      if (unsigned id = add->second.second >> 1)
-		gimple_call_set_arg (mapping.first, mapping.second.second + 2, 
+	      if (unsigned id = add->second.count)
+		gimple_call_set_arg (mapping.first, mapping.second.count + 2, 
 				     build_int_cst (unsigned_type_node, id));
 	    }
 	}
