48313261bc05f4d314e092517bc5ecfe29cd5672
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-08-21 17:25:22 +0000

    #26919: Fix v_if (i > 0)

---
+++ gcc/testsuite/g++.target/tt/sfpi/gtzero-26919-wh.C
@@ -0,0 +1,16 @@
+// { dg-options "-mcpu=tt-wh -O2 -I [SFPI]/include -fno-exceptions -fno-rtti" }
+
+namespace ckernel{
+extern unsigned *instrn_buffer;
+}
+#include <sfpi.h>
+
+using namespace sfpi;
+
+void bad()
+{
+  vUInt i = l_reg[LRegs::LReg0];
+  v_if (i > 0) {
+  } v_endif;
+}
+// { dg-final { scan-assembler {\n_Z3badv:\n\tSFPSETCC	L0, 0, 4\n\tSFPSETCC	L0, 0, 2\n\tSFPENCC	3, 10\n\tret\n} } }
