4cd389e24e1f6e484edc17d76fd4d0bd2a33ee41
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-04-14 17:48:54 +0000

    #40182: Correct sfp{load,store} mod value checks

---
+++ gcc/testsuite/g++.target/tt/sfpi/dstreg-bh.C
@@ -0,0 +1,41 @@
+// { dg-options "-mcpu=tt-bh-tensix -O2 -I [SFPI]/include -fno-exceptions -fno-rtti" }
+// { dg-final { check-function-bodies "**" "" } }
+
+namespace ckernel{
+    unsigned *instrn_buffer;
+}
+#include <sfpi.h>
+
+using namespace sfpi;
+
+void one () {
+  vFloat a = dst_reg[0];
+  vFloat b = dst_reg[2].mode<SFPLOAD_MOD0_FMT_FP32> (2);
+
+  dst_reg[4] = a;
+  dst_reg[5].mode<SFPLOAD_MOD0_FMT_FP16B> () = b;
+}
+/*
+**_Z3onev:
+**	SFPLOAD	L1, 0, 0, 7
+**	SFPLOAD	L0, 4, 3, 2
+**	SFPSTORE	L1, 8, 0, 7
+**	SFPSTORE	L0, 10, 2, 7
+**	ret
+*/
+
+void two () {
+  vInt a = dst_reg[0];
+  vUInt b = dst_reg[1];
+
+  dst_reg[2] = a;
+  dst_reg[3] = b;
+}
+/*
+**_Z3onev:
+**	SFPLOAD	L1, 0, 0, 7
+**	SFPLOAD	L0, 4, 3, 2
+**	SFPSTORE	L1, 8, 0, 7
+**	SFPSTORE	L0, 10, 2, 7
+**	ret
+*/
