e0acd10c4657736f8d507d66919f23706a15b2c1
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-06-27 10:58:25 +0000

    #47984: muli/addi lv variants

--- gcc/testsuite/g++.target/riscv/tt/tensix/delay-34602-qsr.C
+++ gcc/testsuite/g++.target/riscv/tt/tensix/delay-34602-qsr.C
@@ -6,34 +6,34 @@ extern volatile unsigned iptr[];
 namespace dyn {
 void one () {
   auto f = __builtin_rvtt_sfpload (nullptr, 0, 0, 0, 0, 0);
-  auto y = __builtin_rvtt_sfpmuli (iptr, f, 0, 0, 0, 8);
+  auto y = __builtin_rvtt_sfpmuli (iptr, f, 0, 0, 0, 0);
   __builtin_rvtt_sfpstore (nullptr, y, 0, 0, 0, 0, 0);
 }
 /*
 **_ZN3dyn3oneEv:
 **	SFPLOAD	L0, 0, 0, 0, 0, 0
-**	SFPMULI	L0, 0, 8
+**	SFPMULI	L0, 0, 0
 **	SFPSTORE	L0, 0, 0, 0, 0, 0
 **	ret
 */
 
 void two () {
   auto f = __builtin_rvtt_sfpload (nullptr, 0, 0, 0, 0, 0);
-  auto y = __builtin_rvtt_sfpmuli (iptr, f, 0, 0, 0, 8);
+  auto y = __builtin_rvtt_sfpmuli (iptr, f, 0, 0, 0, 0);
   __builtin_rvtt_sfpstore (nullptr, f, 0, 0, 0, 0, 0);
 }
 /*
 **_ZN3dyn3twoEv:
 **	SFPLOAD	L0, 0, 0, 0, 0, 0
 **	SFPMOV	L1, L0, 2
-**	SFPMULI	L1, 0, 8
+**	SFPMULI	L1, 0, 0
 **	SFPSTORE	L0, 0, 0, 0, 0, 0
 **	ret
 */
 
 void one (unsigned i) {
   auto f = __builtin_rvtt_sfpload (nullptr, 0, 0, 0, 0, 0);
-  auto y = __builtin_rvtt_sfpmuli (iptr, f, i, 0, 0, 8);
+  auto y = __builtin_rvtt_sfpmuli (iptr, f, i, 0, 0, 0);
   __builtin_rvtt_sfpstore (nullptr, y, 0, 0, 0, 0, 0);
 }
 /*
@@ -43,17 +43,17 @@ void one (unsigned i) {
 **	li	a5,16777216
 **	addi	a5,a5,-256
 **	and	a0,a0,a5
-**	li	a5, 1946157064	# 2:74000008
+**	li	a5, 1946157056	# 2:74000000
 **	add	a0,a0,a5
 **	lui	a5,%hi\(iptr\)
-**	sw	a0, %lo\(iptr\)\(a5\)	# 2:SFPMULI	L0, a0, 8
+**	sw	a0, %lo\(iptr\)\(a5\)	# 2:SFPMULI	L0, a0, 0
 **	SFPSTORE	L0, 0, 0, 0, 0, 0
 **	ret
 */
 
 void two (unsigned i) {
   auto f = __builtin_rvtt_sfpload (nullptr, 0, 0, 0, 0, 0);
-  auto y = __builtin_rvtt_sfpmuli (iptr, f, i, 0, 0, 8);
+  auto y = __builtin_rvtt_sfpmuli (iptr, f, i, 0, 0, 0);
   __builtin_rvtt_sfpstore (nullptr, f, 0, 0, 0, 0, 0);
 }
 /*
@@ -64,17 +64,17 @@ void two (unsigned i) {
 **	li	a5,16777216
 **	addi	a5,a5,-256
 **	and	a0,a0,a5
-**	li	a5, 1946157080	# 2:74000018
+**	li	a5, 1946157072	# 2:74000010
 **	add	a0,a0,a5
 **	lui	a5,%hi\(iptr\)
-**	sw	a0, %lo\(iptr\)\(a5\)	# 2:SFPMULI	L1, a0, 8
+**	sw	a0, %lo\(iptr\)\(a5\)	# 2:SFPMULI	L1, a0, 0
 **	SFPSTORE	L0, 0, 0, 0, 0, 0
 **	ret
 */
 
 void three () {
   auto f = __builtin_rvtt_sfpload (nullptr, 0, 0, 0, 0, 0);
-  auto y = __builtin_rvtt_sfpmuli (iptr, f, 0, 0, 0, 8);
+  auto y = __builtin_rvtt_sfpmuli (iptr, f, 0, 0, 0, 0);
   __builtin_rvtt_sfpwritelreg (y, 3);
   __builtin_rvtt_sfpstore (nullptr, f, 0, 0, 0, 0, 0);
 }
@@ -82,7 +82,7 @@ void three () {
 **_ZN3dyn5threeEv:
 **	SFPLOAD	L0, 0, 0, 0, 0, 0
 **	SFPMOV	L3, L0, 2
-**	SFPMULI	L3, 0, 8
+**	SFPMULI	L3, 0, 0
 **	# WRITE L3
 **	SFPSTORE	L0, 0, 0, 0, 0, 0
 **	ret
