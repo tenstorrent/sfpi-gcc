673c295e1b4228708af8fd580a94c668636cc6ad
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-07-14 17:45:53 +0000

    #40182: Check conversion availability

--- gcc/testsuite/g++.target/riscv/tt/sfpi/conv-bh.C
+++ gcc/testsuite/g++.target/riscv/tt/sfpi/conv-bh.C
@@ -62,3 +62,71 @@ void three () {
 **	SFPSTORE	L0, 8, 0, 7
 **	ret
 */
+
+void available () {
+  l_reg[LRegs::LReg0] = convert<vFloat> (vFloat (0.0f));
+  l_reg[LRegs::LReg0] = convert<vFloat> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vFloat> (vUInt (0));
+  l_reg[LRegs::LReg0] = convert<vFloat> (vSMag (0));
+  l_reg[LRegs::LReg0] = convert<vFloat> (vMag (0));
+
+  //l_reg[LRegs::LReg0] = convert<vInt> (vFloat (0.0f));
+  l_reg[LRegs::LReg0] = convert<vInt> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vInt> (vUInt (0));
+  l_reg[LRegs::LReg0] = convert<vInt> (vSMag (0));
+  l_reg[LRegs::LReg0] = convert<vInt> (vMag (0));
+
+  //l_reg[LRegs::LReg0] = convert<vUInt> (vFloat (0.0f));
+  //l_reg[LRegs::LReg0] = convert<vUInt> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt> (vUInt (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt> (vSMag (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt> (vMag (0));
+
+  //l_reg[LRegs::LReg0] = convert<vSMag> (vFloat (0.0f));
+  l_reg[LRegs::LReg0] = convert<vSMag> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vSMag> (vUInt (0));
+  l_reg[LRegs::LReg0] = convert<vSMag> (vSMag (0));
+  l_reg[LRegs::LReg0] = convert<vSMag> (vMag (0));
+
+  //l_reg[LRegs::LReg0] = convert<vMag> (vFloat (0.0f));
+  //l_reg[LRegs::LReg0] = convert<vMag> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vMag> (vUInt (0));
+  //l_reg[LRegs::LReg0] = convert<vMag> (vSMag (0));
+  l_reg[LRegs::LReg0] = convert<vMag> (vMag (0));
+
+  l_reg[LRegs::LReg0] = convert<vFloat16a> (vFloat (0.0f));
+  l_reg[LRegs::LReg0] = convert<vFloat16a> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vFloat16a> (vUInt (0));
+  l_reg[LRegs::LReg0] = convert<vFloat16a> (vSMag (0));
+  l_reg[LRegs::LReg0] = convert<vFloat16a> (vMag (0));
+
+  l_reg[LRegs::LReg0] = convert<vFloat16b> (vFloat (0.0f));
+  l_reg[LRegs::LReg0] = convert<vFloat16b> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vFloat16b> (vUInt (0));
+  l_reg[LRegs::LReg0] = convert<vFloat16b> (vSMag (0));
+  l_reg[LRegs::LReg0] = convert<vFloat16b> (vMag (0));
+
+  l_reg[LRegs::LReg0] = convert<vUInt16> (vFloat (0.0f));
+  //l_reg[LRegs::LReg0] = convert<vUInt16> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt16> (vUInt (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt16> (vSMag (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt16> (vMag (0));
+
+  l_reg[LRegs::LReg0] = convert<vUInt8> (vFloat (0.0f));
+  //l_reg[LRegs::LReg0] = convert<vUInt8> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt8> (vUInt (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt8> (vSMag (0));
+  //l_reg[LRegs::LReg0] = convert<vUInt8> (vMag (0));
+
+  l_reg[LRegs::LReg0] = convert<vSMag16> (vFloat (0.0f));
+  //l_reg[LRegs::LReg0] = convert<vSMag16> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vSMag16> (vUInt (0));
+  //l_reg[LRegs::LReg0] = convert<vSMag16> (vSMag (0));
+  //l_reg[LRegs::LReg0] = convert<vSMag16> (vMag (0));
+
+  l_reg[LRegs::LReg0] = convert<vSMag8> (vFloat (0.0f));
+  //l_reg[LRegs::LReg0] = convert<vSMag8> (vInt (0));
+  //l_reg[LRegs::LReg0] = convert<vSMag8> (vUInt (0));
+  //l_reg[LRegs::LReg0] = convert<vSMag8> (vSMag (0));
+  //l_reg[LRegs::LReg0] = convert<vSMag8> (vMag (0));
+}
