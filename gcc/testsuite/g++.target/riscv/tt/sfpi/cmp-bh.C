a96e954c763afd7d59885d85265db6c2c2e472b4
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-08-14 18:37:28 +0000

    Correct sfpxiadd compc

--- gcc/testsuite/g++.target/riscv/tt/sfpi/cmp-bh.C
+++ gcc/testsuite/g++.target/riscv/tt/sfpi/cmp-bh.C
@@ -90,3 +90,125 @@ void two () {
 **	SFPSTORE	L0, 4, 4, 7
 **	ret
 */
+
+void fp16b () {
+  vFloat a = dst_reg[0];
+  vInt b = dst_reg[1];
+
+  v_if (b < 0x7f80000) {
+    a = 0;
+  } v_endif;
+  
+
+  dst_reg[0] = a;
+}
+/*
+**_Z5fp16bv:
+**	SFPLOAD	L0, 0, 0, 7
+**	SFPLOAD	L2, 2, 4, 7
+**	SFPLOADI	L1, 2040, 0
+**	SFPIADD	L1, L2, 0, 2
+**	SFPMOV	L0, L9, 0	# LV:L0
+**	SFPENCC	3, 10
+**	SFPSTORE	L0, 0, 0, 7
+**	ret
+*/
+
+void gt () {
+  vFloat a = dst_reg[0];
+  vInt b = dst_reg[1];
+  vInt c = dst_reg[2];
+
+  v_if (b > 0) {
+    a = 0;
+  } v_elseif (c > 0) {
+    a = 1.0f;
+  } v_endif;
+  
+
+  dst_reg[0] = a;
+}
+/*
+**_Z2gtv:
+**	SFPLOAD	L0, 0, 0, 7
+**	SFPLOAD	L2, 2, 4, 7
+**	SFPLOAD	L1, 4, 4, 7
+**	SFPSETCC	L2, 0, 4
+**	SFPSETCC	L2, 0, 2
+**	SFPMOV	L0, L9, 0	# LV:L0
+**	SFPCOMPC
+**	SFPSETCC	L1, 0, 4
+**	SFPSETCC	L1, 0, 2
+**	SFPMOV	L0, L10, 0	# LV:L0
+**	SFPENCC	3, 10
+**	SFPSTORE	L0, 0, 0, 7
+**	ret
+*/
+
+void le () {
+  vFloat a = dst_reg[0];
+  vInt b = dst_reg[1];
+  vInt c = dst_reg[2];
+
+  v_if (b <= 0) {
+    a = 0;
+  } v_elseif (c <= 0) {
+    a = 1.0f;
+  } v_endif;
+  
+  dst_reg[0] = a;
+}
+/*
+**_Z2lev:
+**	SFPLOAD	L0, 0, 0, 7
+**	SFPLOAD	L2, 2, 4, 7
+**	SFPLOAD	L1, 4, 4, 7
+**	SFPSETCC	L2, 0, 4
+**	SFPSETCC	L2, 0, 2
+**	SFPCOMPC
+**	SFPMOV	L0, L9, 0	# LV:L0
+**	SFPCOMPC
+**	SFPPUSHC	0
+**	SFPSETCC	L1, 0, 4
+**	SFPSETCC	L1, 0, 2
+**	SFPCOMPC
+**	SFPMOV	L0, L10, 0	# LV:L0
+**	SFPPOPC	0
+**	SFPENCC	3, 10
+**	SFPSTORE	L0, 0, 0, 7
+**	ret
+*/
+
+void eqoreq () {
+  vFloat a = dst_reg[0];
+  vInt b = dst_reg[1];
+  vInt c = dst_reg[2];
+
+  v_if (b == 0 || c == 0) {
+    a = 0;
+  } v_elseif (b == 0 || c == 0) {
+    a = 1.0f;
+  } v_endif;
+  
+  dst_reg[0] = a;
+}
+/*
+**_Z6eqoreqv:
+**	SFPLOAD	L0, 0, 0, 7
+**	SFPLOAD	L2, 2, 4, 7
+**	SFPLOAD	L1, 4, 4, 7
+**	SFPSETCC	L2, 0, 2
+**	SFPSETCC	L1, 0, 2
+**	SFPCOMPC
+**	SFPMOV	L0, L9, 0	# LV:L0
+**	SFPCOMPC
+**	SFPPUSHC	0
+**	SFPSETCC	L2, 0, 2
+**	SFPSETCC	L1, 0, 2
+**	SFPCOMPC
+**	SFPMOV	L0, L10, 0	# LV:L0
+**	SFPPOPC	0
+**	SFPENCC	3, 10
+**	SFPSTORE	L0, 0, 0, 7
+**	ret
+*/
