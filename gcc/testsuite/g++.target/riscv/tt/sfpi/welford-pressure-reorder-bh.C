6e59ca6c7728e63ddd1030d3c4c605653e4c134e
Nachiket Kapre <nkapre@tenstorrent.com>
2026-08-13 21:17:58 -0400

    Add Blackhole pressure scheduler fixtures

---
+++ gcc/testsuite/g++.target/riscv/tt/sfpi/welford-pressure-reorder-bh.C
@@ -0,0 +1,5 @@
+// { dg-do compile }
+// { dg-options "-mcpu=tt-bh-tensix -O2 -I [SFPI]/include -fno-exceptions -fno-rtti -mtt-tensix-optimize-lp-schedule -fdump-tree-rvtt_lp_schedule" }
+// { dg-final { scan-tree-dump "SFPU pressure schedule:.*old-peak=9.*new-peak=8.*applied=yes" "rvtt_lp_schedule" } }
+
+#include "welford-pressure-reorder-wh.C"
--- gcc/testsuite/g++.target/riscv/tt/sfpi/welford-pressure-wh.C
+++ gcc/testsuite/g++.target/riscv/tt/sfpi/welford-pressure-wh.C
@@ -1,3 +1,4 @@
+// { dg-do compile }
 // { dg-options "-mcpu=tt-wh-tensix -O2 -I [SFPI]/include -fno-exceptions -fno-rtti -mtt-tensix-optimize-lp-schedule -fdump-tree-rvtt_lp_schedule" }
 // { dg-final { scan-tree-dump "SFPU pressure region:.*ops=16.*live-in=7.*peak=8" "rvtt_lp_schedule" } }
 
