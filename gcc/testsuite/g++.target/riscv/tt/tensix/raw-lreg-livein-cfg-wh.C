4f234412dcc9efb9309cb1127ec637ffb5b19582
Nachiket Kapre <nkapre@tenstorrent.com>
2026-08-15 10:42:48 +0000

    testsuite: cover raw LREG loops and CFG joins

--- gcc/testsuite/g++.target/riscv/tt/tensix/raw-lreg-livein-cfg-wh.C
+++ gcc/testsuite/g++.target/riscv/tt/tensix/raw-lreg-livein-cfg-wh.C
@@ -42,7 +42,50 @@ raw_lreg_last_use_contract ()
   __builtin_rvtt_sfpwritelreg (result, 4);
 }
 
+void
+raw_lreg_live_across_loop (int count)
+{
+  __builtin_rvtt_sfprawlreg_access (0, 0x02);
+  auto l0 = __builtin_rvtt_sfpreadlreg (0);
+  auto l2 = __builtin_rvtt_sfpreadlreg (2);
+  auto l3 = __builtin_rvtt_sfpreadlreg (3);
+  auto l4 = __builtin_rvtt_sfpreadlreg (4);
+  auto l5 = __builtin_rvtt_sfpreadlreg (5);
+  auto l6 = __builtin_rvtt_sfpreadlreg (6);
+  while (count-- > 0)
+    (void) __builtin_rvtt_sfpload (0, 0, 0, 0, 0, 0);
+  auto l1 = __builtin_rvtt_sfpreadlreg (1);
+  auto result = __builtin_rvtt_sfpmad (l1, l0, l2, 0);
+  result = __builtin_rvtt_sfpmad (result, l3, l4, 0);
+  result = __builtin_rvtt_sfpmad (result, l5, l6, 0);
+  __builtin_rvtt_sfpwritelreg (result, 4);
+}
+
+void
+raw_lreg_live_across_multi_pred (int outer, int inner)
+{
+  __builtin_rvtt_sfprawlreg_access (0, 0x02);
+  auto l0 = __builtin_rvtt_sfpreadlreg (0);
+  auto l2 = __builtin_rvtt_sfpreadlreg (2);
+  auto l3 = __builtin_rvtt_sfpreadlreg (3);
+  auto l4 = __builtin_rvtt_sfpreadlreg (4);
+  auto l5 = __builtin_rvtt_sfpreadlreg (5);
+  auto l6 = __builtin_rvtt_sfpreadlreg (6);
+  if (outer)
+    {
+      if (inner)
+        (void) __builtin_rvtt_sfpload (0, 0, 0, 0, 0, 0);
+    }
+  else
+    (void) __builtin_rvtt_sfpload (0, 0, 0, 0, 0, 0);
+  auto l1 = __builtin_rvtt_sfpreadlreg (1);
+  auto result = __builtin_rvtt_sfpmad (l1, l0, l2, 0);
+  result = __builtin_rvtt_sfpmad (result, l3, l4, 0);
+  result = __builtin_rvtt_sfpmad (result, l5, l6, 0);
+  __builtin_rvtt_sfpwritelreg (result, 4);
+}
+
 // { dg-final { scan-assembler "# RAWLREG 0, 2" } }
 // { dg-final { scan-assembler "# RAWLREG 2, 128" } }
-// { dg-final { scan-assembler-times "SFPLOAD\\tL[0234567]," 2 } }
+// { dg-final { scan-assembler-times "SFPLOAD\\tL[0234567]," 4 } }
 // { dg-final { scan-assembler-not "SFPLOAD\\tL1," } }
