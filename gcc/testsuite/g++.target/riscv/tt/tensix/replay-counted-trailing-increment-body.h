9a5404d4f227ff943d2521e6bb4d1d03ce167bd4
Nachiket Kapre <nkapre@tenstorrent.com>
2026-08-15 20:56:40 +0000

    riscv: replay counted SFPU payload before Dst increment

---
+++ gcc/testsuite/g++.target/riscv/tt/tensix/replay-counted-trailing-increment-body.h
@@ -0,0 +1,13 @@
+void counted_replay_trailing_increment ()
+{
+  auto x = __builtin_rvtt_sfpreadlreg (0);
+  for (unsigned ix = 0; ix != 8; ++ix)
+    {
+      x = __builtin_rvtt_sfpmul (x, x, 0);
+      x = __builtin_rvtt_sfpmul (x, x, 0);
+      x = __builtin_rvtt_sfpmul (x, x, 0);
+      x = __builtin_rvtt_sfpmul (x, x, 0);
+      __builtin_rvtt_ttincrwc (0, 2, 0, 0);
+    }
+  __builtin_rvtt_sfpwritelreg (x, 0);
+}
