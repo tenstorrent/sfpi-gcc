bf0eae6c797dfb02944be8cbaecd4bbc9ba83d0b
Nachiket Kapre <nkapre@tenstorrent.com>
2026-08-25 08:24:38 -0400

    Revert "const-residency: require strict-positive loop savings"
    This reverts commit 437162abc30aee8cbfab186c5199062acdf3c2b8.

--- gcc/testsuite/g++.target/riscv/tt/tensix/const-residency-runtime-trip-refuse-bh.C
+++ gcc/testsuite/g++.target/riscv/tt/tensix/const-residency-runtime-trip-refuse-bh.C
@@ -2,7 +2,7 @@
 // not prove even the two trips needed when a two-word materialization folds
 // directly into an arithmetic consumer.  Renamed/signed-bound twin included.
 // { dg-options "-mcpu=tt-bh-tensix -O2 -fno-unroll-loops -mtt-tensix-optimize-const-residency -fdump-tree-rvtt_prgm_const-details" }
-// { dg-final { scan-tree-dump-times "loop-profitability-unproven: strict-positive needs 2 proven trips; 2 materialization words, 0 resident read words, 3 programming words" 2 "rvtt_prgm_const" } }
+// { dg-final { scan-tree-dump-times "loop-profitability-unproven: break-even needs 2 proven trips; 2 materialization words, 0 resident read words, 3 programming words" 2 "rvtt_prgm_const" } }
 // { dg-final { scan-tree-dump-not "const-residency: allocated PRGM" "rvtt_prgm_const" } }
 // { dg-final { scan-assembler-not "SFPCONFIG" } }
 
