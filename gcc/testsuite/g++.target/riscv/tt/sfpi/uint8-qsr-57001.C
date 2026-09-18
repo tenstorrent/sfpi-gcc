36a46f7464d9ebd1cc4076070cd949aa72935f3f
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-09-18 21:13:11 +0000

    #57028: sfpcast pair elision

--- gcc/testsuite/g++.target/riscv/tt/sfpi/uint8-qsr-57001.C
+++ gcc/testsuite/g++.target/riscv/tt/sfpi/uint8-qsr-57001.C
@@ -13,6 +13,17 @@ void one () {
     dst_reg[1].mode<DataLayout::I32> () = a; }
   { vInt a = dst_reg[0].mode<DataLayout::SM8> ();
     dst_reg[1].mode<DataLayout::SM8> () = a; }
+}
+/*
+**_Z3onev:
+**	SFPLOAD	L0, 0, 11, 7, 0, 0
+**	SFPSTORE	L0, 2, 4, 7, 0, 0
+**	SFPLOAD	L0, 0, 5, 7, 0, 0
+**	SFPSTORE	L0, 2, 5, 7, 0, 0
+**	ret
+*/
+
+void two () {
   { vUInt a = dst_reg[0].mode<DataLayout::U8> ();
     dst_reg[1].mode<DataLayout::U8> () = a; }
   { vSMag a = dst_reg[0].mode<DataLayout::U8> ();
@@ -21,13 +32,7 @@ void one () {
     dst_reg[1].mode<DataLayout::SM8> () = a; }
 }
 /*
-**_Z3onev:
-**	SFPLOAD	L0, 0, 11, 7, 0, 0
-**	SFPSTORE	L0, 2, 4, 7, 0, 0
-**	SFPLOAD	L0, 0, 5, 7, 0, 0
-**	SFPCAST	L0, L0, 2
-**	SFPCAST	L0, L0, 3
-**	SFPSTORE	L0, 2, 5, 7, 0, 0
+**_Z3twov:
 **	SFPLOAD	L0, 0, 11, 7, 0, 0
 **	SFPSTORE	L0, 2, 11, 7, 0, 0
 **	SFPLOAD	L0, 0, 11, 7, 0, 0
