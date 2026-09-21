38ee8c359998010905efee98843e04ac5c1233be
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-09-21 20:25:38 +0000

    More SM8,U8 capability

--- gcc/testsuite/g++.target/riscv/tt/sfpi/uint8-qsr-57001.C
+++ gcc/testsuite/g++.target/riscv/tt/sfpi/uint8-qsr-57001.C
@@ -8,36 +8,62 @@ namespace ckernel{
 
 using namespace sfpi;
 
-void one () {
+void vint () {
   { vInt a = dst_reg[0].mode<DataLayout::U8> ();
-    dst_reg[1].mode<DataLayout::I32> () = a; }
+    l_reg[LRegs::LReg0] = a; }
+  { vInt a = l_reg[LRegs::LReg0];
+    dst_reg[1].mode<DataLayout::U8> () = a; }
   { vInt a = dst_reg[0].mode<DataLayout::SM8> ();
+    l_reg[LRegs::LReg0] = a; }
+  { vInt a = l_reg[LRegs::LReg0];
     dst_reg[1].mode<DataLayout::SM8> () = a; }
 }
 /*
-**_Z3onev:
+**_Z4vintv:
 **	SFPLOAD	L0, 0, 11, 7, 0, 0
-**	SFPSTORE	L0, 2, 4, 7, 0, 0
+**	# WRITE L0
+**	# READ L0
+**	SFPSTORE	L0, 2, 11, 7, 0, 0
 **	SFPLOAD	L0, 0, 5, 7, 0, 0
+**	SFPCAST	L0, L0, 2
+**	# WRITE L0
+**	# READ L0
+**	SFPCAST	L0, L0, 3
 **	SFPSTORE	L0, 2, 5, 7, 0, 0
 **	ret
 */
 
-void two () {
+void vuint () {
   { vUInt a = dst_reg[0].mode<DataLayout::U8> ();
+    l_reg[LRegs::LReg0] = a; }
+  { vUInt a = l_reg[LRegs::LReg0];
     dst_reg[1].mode<DataLayout::U8> () = a; }
+}
+/*
+**_Z5vuintv:
+**	SFPLOAD	L0, 0, 11, 7, 0, 0
+**	# WRITE L0
+**	# READ L0
+**	SFPSTORE	L0, 2, 11, 7, 0, 0
+**	ret
+*/
+
+void vsmag () {
   { vSMag a = dst_reg[0].mode<DataLayout::U8> ();
-    dst_reg[1].mode<DataLayout::SM32> () = a; }
-  { vSMag a = dst_reg[0].mode<DataLayout::SM8> ();
+    l_reg[LRegs::LReg0] = a; }
+
+ { vSMag a = dst_reg[0].mode<DataLayout::SM8> ();
+    l_reg[LRegs::LReg0] = a; }
+  { vSMag a = l_reg[LRegs::LReg0];
     dst_reg[1].mode<DataLayout::SM8> () = a; }
 }
 /*
-**_Z3twov:
+**_Z5vsmagv:
 **	SFPLOAD	L0, 0, 11, 7, 0, 0
-**	SFPSTORE	L0, 2, 11, 7, 0, 0
-**	SFPLOAD	L0, 0, 11, 7, 0, 0
-**	SFPSTORE	L0, 2, 4, 7, 0, 0
+**	# WRITE L0
 **	SFPLOAD	L0, 0, 5, 7, 0, 0
+**	# WRITE L0
+**	# READ L0
 **	SFPSTORE	L0, 2, 5, 7, 0, 0
 **	ret
 */
