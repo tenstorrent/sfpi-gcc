cc12aa986e2cf2fad348d859f6c40d3fb378177d
n <n@t>
2026-09-26 13:45:18 +0000

    TEMP diag

--- gcc/config/riscv/tt/gimple-rvtt-crosscall.cc
+++ gcc/config/riscv/tt/gimple-rvtt-crosscall.cc
@@ -1372,7 +1372,7 @@ callee_body_ok_p (function *fn, const auto_vec<contract_entry> &contract,
   for (const contract_entry &e : contract)
     loads.safe_push (e.load);
   if (!rvtt_pressure_loop_legal_p (consumer_loop, loads,
-				   /*report=*/false,
+				   /*report=*/true,   /* XXX TEMP: diagnosing crosscall-callee-pressure */
 				   /*cc_transients=*/false,
 				   /*exempt_creg_reads=*/
 				   !config.is_empty ()))
